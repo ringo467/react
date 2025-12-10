fd098173c11d8450e559068c228963765cca9071
Student <student@Administrators-Mac-mini-2.local>
2025-12-10 11:42:03 +0200

    tunnitoo

---
+++ expenses/src/Header.jsx
@@ -0,0 +1,18 @@
+function Header(){
+
+    return(
+        <header>
+            <h1>My website</h1>
+            <nav>
+                <ul>
+                    <li><a href="">Home</li></a>
+                    <li><a href="">About</li></a>
+                    <li><a href="">Services</li></a>
+                    <li><a href="">Contact</li></a>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header
